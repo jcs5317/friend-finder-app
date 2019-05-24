549e01b5648eff70cdd7e275872512ea755f7e3d
jordan <jcs5317@gmail.com>
2019-05-24 14:49:32 -0700

    correct html

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -15,4 +15,52 @@
     }
 };
 
+// Capture the form inputs 
+$("#submit").on("click", function () {
+    event.preventDefault();
 
+    var formIsValid = $('#friendForm')[0].checkValidity();
+    console.log(formIsValid)
+    var img = $("#photo").val().trim();
+    var urlIsValid = validURL(img);
+
+    if(formIsValid && urlIsValid) {
+        var newFriend = {
+            name: $("#name").val().trim(),
+            photo: img,
+            scores: []
+        }
+
+        $('select.chosen-select').each(function(i, elem){
+            newFriend.scores.push(parseInt(elem.value))
+            elem.value = "";
+        });
+
+        $("#name, #photo").val('');
+
+        // console.log(newFriend)
+        $.post('/api/friends', newFriend, function(data){
+            $("#match-name, #match-img").empty();
+
+            $("#match-name").text(data.name);
+            $('#match-img').attr("src", data.photo);
+
+            // Show the modal with the top match 
+            $("#results-modal").modal('toggle');
+        });
+
+    } else {
+        return alert('check the form and try again!')
+    }
+
+});
+
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+}
