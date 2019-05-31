d98092b6fd3233264e967e45444d5d14adadd315
jordan <jcs5317@gmail.com>
2019-05-31 15:08:50 -0700

    validity update and js link

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -1,20 +1,3 @@
-  // Chosen CSS
-  var config = {
-    '.chosen-select': {},
-    '.chosen-select-deselect': {
-        allow_single_deselect: true
-    },
-    '.chosen-select-no-single': {
-        disable_search_threshold: 10
-    },
-    '.chosen-select-no-results': {
-        no_results_text: 'Oops, nothing found!'
-    },
-    '.chosen-select-width': {
-        width: "95%"
-    }
-};
-
 // Capture the form inputs 
 $("#submit").on("click", function () {
     event.preventDefault();
@@ -56,11 +39,7 @@ $("#submit").on("click", function () {
 });
 
 function validURL(str) {
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    var pattern = new RegExp(/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
+); // fragment locator
     return !!pattern.test(str);
 }
